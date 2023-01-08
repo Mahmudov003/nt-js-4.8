b0121f45ab16159f52fae8c39846df08086c5e00
Asliddin Mahmudov <asliddinmakhmudov003@gmail.com>
2023-01-08 22:01:21 +0500

    2endi

--- clock/script.js
+++ clock/script.js
@@ -1,34 +1,43 @@
 //1. Elektron soat yasang. Har sekundda qiymat yangilanib tursin (setInterval dan foydalaning)
 
-let card = document.getElementById('card')
+let card = document.getElementById("card");
 setInterval(() => {
-    let today = new Date();
-    let hours = today.getHours()
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
-    card.innerHTML = `${hours}:${minutes}:${seconds}`;
-}, 1000)
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+  card.innerHTML = `${hours}:${minutes}:${seconds}`;
+}, 1000);
 
 //2. Timer yasang. 1 Minutdan boshlab orqaga sanasin. Sichqoncha timerni ustiga
 // borganda sanashni to'xtatib tursin, ustidan chiqib ketganda, sanashni davom ettirsin
 
-const cardTime = document.getElementById('card2');
+const cardTime = document.getElementById("card2");
 let second = 60;
-const interval = setInterval(() => {
-        second--
-        second ===0 ? clearInterval(interval):
 
+function createInterval() {
+    return setInterval(() => {
+        second--;
+        if (second === 0) clearInterval(interval);
         cardTime.innerHTML = `${second}`;
-        cardTime.addEventListener('focus', () =>clearInterval(interval))
-        return interval;
-    },
-    1000);
+      }, 1000);
+}
+
+let interval = createInterval()
 
 
+cardTime.addEventListener("mouseenter", () => {
+    console.log('mouse enter')
+    clearInterval(interval)
+});
+cardTime.addEventListener("mouseleave", () => {
+    console.log('mouse leave')
+    interval = createInterval()
+});
 
 //3. Argumentdan berilgan sonlardan eng kichigini qaytaruvchi funksiya yozing.
 function minim(...arg) {
-    return Math.min(...arg)
+  return Math.min(...arg);
 }
-const minValue = minim(22,23,34,55,6554,5432,232,87);
-console.log(minValue)
+const minValue = minim(22, 23, 34, 55, 6554, 5432, 232, 87);
+console.log(minValue);
